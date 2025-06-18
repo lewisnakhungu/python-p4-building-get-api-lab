936ac84ef1a251fcbe42d5e9956ecd941b46024a
Lewis Robert Nakhungu <lokiyule@gmail.com>
2025-06-18 18:12:31 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -18,21 +18,48 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
+@app.route('/bakeries', methods=['GET'])
 def bakeries():
-    return ''
+#    class Bakery(db.Model, SerializerMixin):
+#     __tablename__ = 'bakeries'
 
-@app.route('/bakeries/<int:id>')
+#     serialize_rules = ('-baked_goods.bakery',)
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     created_at = db.Column(db.DateTime, server_default=db.func.now())
+#     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+#     baked_goods = db.relationship('BakedGood', backref='bakery')
+
+    
+    bakeries = [bakery.to_dict()  for bakery in Bakery.query.all()]
+
+    response = make_response(bakeries), 200
+    return response
+       
+
+@app.route('/bakeries/<int:id>', methods=['GET'])
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    if not bakery:
+        return make_response({"error":"Bakery not found"}, 404)
+    bakery_dict = bakery.to_dict()
+    response = make_response(bakery_dict, 200)
+    return response
 
-@app.route('/baked_goods/by_price')
+@app.route('/baked_goods/by_price', methods=['GET'])
 def baked_goods_by_price():
-    return ''
+    goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return make_response([good.to_dict() for good in goods], 200)
+    
 
-@app.route('/baked_goods/most_expensive')
+    
+
+@app.route('/baked_goods/most_expensive', methods=['GET'])
 def most_expensive_baked_good():
-    return ''
-
+    good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return make_response(good.to_dict(), 200)
+ 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
